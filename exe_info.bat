ac93c14c75d4c4134a13791fb27a65b8717a8807
CanadaHonk <oj@oojmed.com>
2022-09-01 22:36:33 +0100

    exe info: change description to appease Defender
    it somehow marks it as a trojan if the github link is in the exe metadata. seriously?

--- exe_info.bat
+++ exe_info.bat
@@ -1,1 +1,1 @@
-.\rcedit.exe capybara.exe --set-version-string ProductVersion %1 --set-version-string ProductName Capybara --set-version-string FileDescription "https://github.com/OpenAsar/capybara" --set-file-version %1 --set-product-version %1
+.\rcedit.exe capybara.exe --set-version-string ProductVersion %1 --set-version-string ProductName Capybara --set-version-string FileDescription "OpenAsar Squirrel replacement" --set-file-version %1 --set-product-version %1
